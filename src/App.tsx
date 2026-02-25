3f4a8c42b7954ca0552b450dc12c9f00b9794a41
selahssea <selahssea@gmail.com>
2026-02-24 19:05:25 -0800

    Style adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,17 @@ import {
 
 export default function App() {
   return (
-    <div className="flex h-[100dvh] flex-col divide-y divide-teal-300 dark:divide-teal-300/10 dark:bg-teal-950 dark:caret-yellow-500">
+    <div className="flex h-[100dvh] flex-col border-t-1 border-teal-300 dark:border-teal-300/10 divide-y divide-teal-300 dark:divide-teal-300/10 dark:bg-teal-950 dark:caret-yellow-500">
       <header className="flex h-14 shrink-0 divide-x divide-teal-300 dark:divide-teal-300/10">
         <a
-          className="flex aspect-square h-full items-center justify-center hover:bg-teal-800"
+          className="flex aspect-square h-full items-center justify-center hover:bg-teal-300/30 dark:hover:bg-teal-800"
           href="/"
           title="Lame Local LLM"
         >
           <img
             src="/llllm.svg"
             className="w-8 mr-1 mt-0.5 dark:opacity-70"
-            alt=""
+            alt="LLLLM"
           />
         </a>
         <div className="grow items-center justify-end gap-3 px-4 py-2 sm:flex"></div>
